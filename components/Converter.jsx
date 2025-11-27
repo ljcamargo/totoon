ed9d745ba1e604ffe615553e7ac93b55d50723be
Luis J Camargo <lsjcp@yahoo.com>
2025-11-26 19:25:42 -0600

    kofi fix

--- components/Converter.jsx
+++ components/Converter.jsx
@@ -610,7 +610,7 @@ const Converter = () => {
 
             {/* Donation */}
             <div className="mt-16 flex justify-center pb-8">
-                <a href='https://ko-fi.com/F1F6169085' target='_blank' rel="noopener noreferrer">
+                <a href='https://ko-fi.com/ljcamargo' target='_blank' rel="noopener noreferrer">
                     <img height='36' style={{ border: 0, height: '36px' }} src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
                 </a>
             </div>
